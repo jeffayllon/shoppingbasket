45811d3d77f8007d01e99979fcbfb668f48ca553
Jeff Ayllon <39107488+jeffayllon@users.noreply.github.com>
2020-11-05 11:45:41 +0800

    Update ProductController.java

--- src/main/java/com/example/servingwebcontent/ProductController.java
+++ src/main/java/com/example/servingwebcontent/ProductController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+/*
+	author: jeff ayllon
+	date: November 2, 2020
+*/
 @Controller	
 @RequestMapping(path="/product")
 public class ProductController {
