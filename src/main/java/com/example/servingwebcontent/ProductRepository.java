7e6e78618a8ae6f71092ebb1afff5b305e4d7796
Jeff Ayllon <39107488+jeffayllon@users.noreply.github.com>
2020-11-05 11:45:56 +0800

    Update ProductRepository.java

--- src/main/java/com/example/servingwebcontent/ProductRepository.java
+++ src/main/java/com/example/servingwebcontent/ProductRepository.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/*
+	author: jeff ayllon
+	date: November 2, 2020
+*/
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	List<Product> findAll();
